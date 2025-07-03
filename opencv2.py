07d09833207dbd4b413089e1db6f47029ff6dae3
rgrgzz <ict_701@naver.com>
2025-07-04 01:33:07 +0900

    .

--- opencv2.py
+++ opencv2.py
@@ -264,3 +264,422 @@ cv2.imshow('img', img)
 cv2.imshow('dst', dst)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+
+
+# (jupyter 상에서 실행 X) 동영상
+# 고정 크기로 설정 - ipython 상에서 터미널에 직접 입력하여 실행 가능
+import cv2
+
+cap = cv2.VideoCapture('./video.mp4')
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    frame_resized = cv2.resize(frame, (400, 500))
+    cv2.imshow('frame_resized', frame_resized)
+
+    if cv2.waitKey(30) & 0xFF == ord('q'):   # 'q'키로 종료
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+# 비율로 설정 - interpolation 적용 - ipython 상에서 터미널에 직접 입력하여 실행 가능
+import cv2
+
+cap = cv2.VideoCapture('./video.mp4')
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    frame_resized = cv2.resize(frame, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
+    cv2.imshow('frame_resized', frame_resized)
+
+    if cv2.waitKey(30) & 0xFF == ord('q'):   # 'q'키로 종료
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+# 영역을 잘라서 새로운 윈도우(창)에 표시
+import cv2
+
+img = cv2.imread('./img.jpg')
+# img.shape # (390, 640, 3)
+
+crop = img[100:200, 200:400] # 세로 기준 100 : 200 까지, 가로 기준 200 : 400 까지 자름
+
+cv2.imshow('img', img) # 원본 이미지
+cv2.imshow('crop', crop) # 잘린 이미지
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+# 영역을 잘라서 기존 윈도우에 표시
+import cv2
+
+img = cv2.imread('./img.jpg')
+
+crop = img[100:200, 200:400] # 세로 기준 100 : 200 까지, 가로 기준 200 : 400 까지 자름
+img[100:200, 400:600] = crop
+
+cv2.imshow('img', img) # 원본 이미지
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+# 이미지 대칭 Flip
+# 좌우대칭 - 미러반사(거울반사)
+import cv2
+
+img = cv2.imread('./img.jpg')
+flip_horizontal = cv2.flip(img, 1) # flipCode > 0 : 좌우 대칭 Horizontal
+
+cv2.imshow('img', img)
+cv2.imshow('flip_horizontal', flip_horizontal)
+cv2.waitKey(0)
+
+
+
+
+# 상하대칭 - V-미러 반사
+import cv2
+
+img = cv2.imread('./img.jpg')
+flip_vertical = cv2.flip(img, 0) # flipCode == 0 : 상하 대칭 Vertical
+
+cv2.imshow('img', img)
+cv2.imshow('flip_vertical', flip_vertical)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+# 상하좌우 대칭 - 180도 회전
+import cv2
+
+img = cv2.imread('./img.jpg')
+flip_both = cv2.flip(img, -1) # flipCode < 0 : 상하좌우 대칭
+
+cv2.imshow('img', img)
+cv2.imshow('flip_both', flip_both)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+# 시계방향 90도 회전
+import cv2
+
+img = cv2.imread('./img.jpg')
+
+rotate_180 = cv2.rotate(img, cv2.ROTATE_180) # 180도 회전
+
+cv2.imshow('img', img)
+cv2.imshow('rotate_180', rotate_180)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+# 시계 반대방향 90도 회전(시계 방향 270도 회전)
+import cv2
+
+img = cv2.imread('./img.jpg')
+
+rotate_270 = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE) # 시계 반대 방향으로 90도
+
+cv2.imshow('img', img)
+cv2.imshow('rotate_270', rotate_270)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+# 이미지 흑백으로 읽기
+import cv2
+
+img = cv2.imread('./img.jpg', cv2.IMREAD_GRAYSCALE)
+cv2.imshow('img', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+# 불러온 이미지를 흑백으로 변경
+import cv2
+
+img = cv2.imread('./img.jpg')
+
+dst = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow('img', img)
+cv2.imshow('dst', dst)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+# 이미지 변형(흐림) - 가우시안 블러
+# 커널 사이즈 변화에 따른 흐림
+import cv2
+
+img = cv2.imread('./img.jpg')
+
+# (3, 3), (5, 5), (7, 7)
+kernel_3 = cv2.GaussianBlur(img, (3, 3), 0)
+kernel_5 = cv2.GaussianBlur(img, (5, 5), 0)
+kernel_7 = cv2.GaussianBlur(img, (7, 7), 0)
+
+cv2.imshow('img', img)
+cv2.imshow('kernel_3', kernel_3)
+cv2.imshow('kernel_5', kernel_5)
+cv2.imshow('kernel_7', kernel_7)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+# 표준편차 변화에 따른 흐림
+import cv2
+
+img = cv2.imread('./img.jpg')
+
+sigma_1 = cv2.GaussianBlur(img, (0, 0), 1) # sigmaX - 가우시안 커널의 x 방향의 표준 편차
+sigma_2 = cv2.GaussianBlur(img, (0, 0), 2)
+sigma_3 = cv2.GaussianBlur(img, (0, 0), 3)
+
+cv2.imshow('img', img)
+cv2.imshow('sigma_1', sigma_1)
+cv2.imshow('sigma_2', sigma_2)
+cv2.imshow('sigma_3', sigma_3)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+# 사다리꼴 이미지 펼치기
+import cv2
+import numpy as np
+
+img = cv2.imread('./newspaper.jpg')
+
+width, height = 640, 240 # 가로 크기 640, 세로 크기 240 으로 결과물 출력
+
+src = np.array([[511, 352], [1008, 345], [1122, 584], [455, 594]], dtype=np.float32) # Input 4개 지점
+dst = np.array([[0, 0], [width, 0], [width, height], [0, height]], dtype=np.float32) # Output 4개 지점
+# 좌상, 우상, 우하, 좌하 (시계 방향으로 4 지점 정의)
+
+matrix = cv2.getPerspectiveTransform(src, dst) # Matrix 얻어옴
+result = cv2.warpPerspective(img, matrix, (width, height)) # matrix 대로 변환을 함
+
+cv2.imshow('img', img)
+cv2.imshow('result', result)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+# 사다리꼴 이미지 복원하기
+import cv2
+import numpy as np
+
+img = cv2.imread('./newspaper.jpg')
+
+width, height = 640, 240
+
+src = np.array([[511, 352], [1008, 345], [1122, 584], [455, 594]], dtype=np.float32)
+dst = np.array([[0, 0], [width, 0], [width, height], [0, height]], dtype=np.float32)
+
+matrix = cv2.getPerspectiveTransform(src, dst)   
+warped = cv2.warpPerspective(img, matrix, (width, height))
+
+# 복원을 위한 역행렬 (직사각형 → 사다리꼴) - img.shape[1]:가로 - img.shape[0]:세로
+reverse_matrix = cv2.getPerspectiveTransform(dst, src)
+recovered = cv2.warpPerspective(warped, reverse_matrix, (img.shape[1], img.shape[0]))  # 원본 크기로 복원
+
+cv2.imshow('Original', img)
+cv2.imshow('Warped (펼친 이미지)', warped)
+cv2.imshow('Recovered (복원된 이미지)', recovered)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+# 회전된 이미지 올바로 세우기
+import cv2
+import numpy as np
+
+img = cv2.imread('./poker.jpg')
+
+width, height = 530, 710
+
+src = np.array([[702, 143], [1133, 414], [726, 1007], [276, 700]], dtype=np.float32) # Input 4개 지점
+dst = np.array([[0, 0], [width, 0], [width, height], [0, height]], dtype=np.float32) # Output 4개 지점
+# 좌상, 우상, 우하, 좌하 (시계 방향으로 4 지점 정의)
+
+matrix = cv2.getPerspectiveTransform(src, dst) # Matrix 얻어옴
+result = cv2.warpPerspective(img, matrix, (width, height)) # matrix 대로 변환을 함
+
+cv2.imshow('img', img)
+cv2.imshow('result', result)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+# Threshold(임계값)
+import cv2
+
+img = cv2.imread('book.jpg', cv2.IMREAD_GRAYSCALE)
+
+ret, binary = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+
+cv2.imshow('img', img)
+cv2.imshow('binary', binary)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+# Trackbar(0-255값 변화에 따른 변형 확인)
+import cv2
+
+def empty(pos):
+    pass
+
+img = cv2.imread('./book.jpg', cv2.IMREAD_GRAYSCALE)
+
+name = 'Trackbar'
+cv2.namedWindow(name)
+cv2.createTrackbar('threshold', name, 127, 255, empty)
+# bar 이름, 창 이름, 초기값, 최댓값, 이벤트 처리
+
+while True:
+    thresh = cv2.getTrackbarPos('threshold', name)   # bar 이름, 창의 이름
+    ret, binary = cv2.threshold(img, thresh, 255, cv2.THRESH_BINARY)
+    
+    if not ret:
+        break
+
+    cv2.imshow(name, binary)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+cv2.destroyAllWindows()
+
+
+
+
+# 그림판에서 제작한 이미지로 확인
+import cv2
+
+def empty(pos):
+    # print(pos)
+    pass
+
+img = cv2.imread('threshold.png', cv2.IMREAD_GRAYSCALE)
+
+name = 'Trackbar'
+cv2.namedWindow(name)
+
+cv2.createTrackbar('threshold', name, 127, 255, empty)   # bar 이름, 창의 이름, 초기값, 최대값, 이벤트 처리
+
+while True:
+    thresh = cv2.getTrackbarPos('threshold', name)   # bar 이름, 창의 이름
+    ret, binary = cv2.threshold(img, thresh, 255, cv2.THRESH_BINARY)
+
+    if not ret:
+        break
+
+    cv2.imshow('img', img)
+    cv2.imshow(name, binary)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+
+
+
+
+# 색 분류
+import cv2
+
+img = cv2.imread('./threshold.png', cv2.IMREAD_GRAYSCALE)
+
+ret, binary1 = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY)   # 진한 회색, 밝은 회색, 흰색
+ret, binary2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)   # 밝은 회색, 흰색
+ret, binary3 = cv2.threshold(img, 195, 255, cv2.THRESH_BINARY)   # 흰색
+
+cv2.imshow('img', img)
+cv2.imshow('binary1', binary1)
+cv2.imshow('binary2', binary2)
+cv2.imshow('binary3', binary3)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+# Adaptive Threshold - adaptiveThreshold
+import cv2
+
+def empty(pos):
+    # print(pos)
+    pass
+
+img = cv2.imread('book.jpg', cv2.IMREAD_GRAYSCALE)
+
+name = 'Trackbar'
+cv2.namedWindow(name)
+
+# bar 이름, 창의 이름, 초기값, 최대값, 이벤트 처리
+cv2.createTrackbar('block_size', name, 25, 100, empty)   # 홀수만 가능, 1보다는 큰 값
+cv2.createTrackbar('c', name, 3, 10, empty)   # 일반적으로 양수의 값을 사용
+
+while True:
+    block_size = cv2.getTrackbarPos('block_size', name)   # bar 이름, 창의 이름
+    c = cv2.getTrackbarPos('c', name)
+
+    if block_size <= 1:   # 1 이하면 3 으로
+        block_size = 3
+
+    if block_size % 2 == 0:   # 짝수이면 홀수로
+        block_size += 1
+
+    binary = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, block_size, c)
+
+    cv2.imshow(name, binary)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.destroyAllWindows()
